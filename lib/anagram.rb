947aed3ee7c6beb035825387a9c33ded7a64cdea
Nick Ramsey <nramseysc@gmail.com>
2019-07-19 13:51:37 -0700

    pass anagram spec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,21 @@ class String
   # def initialize()
   #
   # end
-  def anagramz
+  def casing
       input_word = self.downcase()
       p input_word
   end
+
+  def anagram(i)
+    gram = self.concat(i).match('(.)\1')[0]
+    if gram = i
+      true
+    end
+  end
 end
 
-p 'Enter first word'
-input = gets.chomp
-input.anagramz
+# p 'Enter first word'
+# input = gets.chomp
+# input.anagramz
 # p 'Enter second word'
 # word2 = gets.chomp
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,12 +2,13 @@ require 'pry'
 require 'anagram.rb'
 
 describe('anagram') do
-  # it('check if two words are anagrams') do
-  #   expect("call, all".anagram?()).to(eq(true))
-  # end
+  it('check if two words are anagrams') do
+    i = 'all'
+    expect('call'.anagram(i)).to(eq(true))
+  end
 
   it('standardize casing for words entered') do
-    expect("CaLl".anagramz()).to(eq("call"))
+    expect('CaLl'.casing()).to(eq('call'))
   end
 
   # it('check if inputted strings are actually words') do
