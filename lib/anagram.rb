a775ac85ddf7e46258770dfb1c783fd47fbbb710
Nick Ramsey <nramseysc@gmail.com>
2019-07-19 13:29:57 -0700

    passes standardized casing spec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,19 @@
 require 'pry'
 require 'fileutils'
-class Class_name
-  def initialize()
 
-  end
-  def method
 
+class String
+  # def initialize()
+  #
+  # end
+  def anagramz
+      input_word = self.downcase()
+      p input_word
   end
 end
+
+p 'Enter first word'
+input = gets.chomp
+input.anagramz
+# p 'Enter second word'
+# word2 = gets.chomp
